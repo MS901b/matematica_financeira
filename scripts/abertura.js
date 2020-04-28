d0e4b7948076303fe6ea38aa6d45dcbb11d211d2
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-04-28 14:22:19 -0300

    update popup

--- scripts/abertura.js
+++ scripts/abertura.js
@@ -73,7 +73,7 @@ function analisaAmbiente()
 	{
 		var conteudo =
 			'<p><strong>Erro de execução – Não abra esse arquivo html diretamente.</strong></p>'
-			+ '<p>Este software deve ser visualizado de um servidor na internet ou utilizando o programa AcessarProdutos fornecido com ele.</p>'
+			+ '<p>Este software deve ser utilizado online, através do <a target="blank"; href="http://m3.ime.unicamp.br"> Matemática Multimídia</a>.</p>'
 			+ '<p><a id="fechar_desesperado" style="float:right; href="#">Fechar</a></p>'
 			+ '<br class="limpador" />';
 
