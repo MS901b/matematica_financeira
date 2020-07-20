f36f1740273f0a4852bd008a3c947291f7b96f76
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-07-20 11:53:36 -0300

    corrido a2p1

--- scripts_software/atividade2_conteudo.js
+++ scripts_software/atividade2_conteudo.js
@@ -1,5 +1,5 @@
 /*
-	Padronização do ID: 
+	Padronização do ID:
 		- [['p/parte','q/questao','/item'],'_'] vai gerar um id do tipo p1_q2_1
 		- [['p/parte','q/questao','/itemletra'],'_'] vai gerar um id do tipo p1_q2_a
 	Palavras-chave: questao, parte, item, itemletra, subitem
@@ -12,20 +12,20 @@ var IdPadrao = [['parte/parte','q/questao','/itemletra','/subitem'],'_'];
 
 /*
 	Questoes
-	
+
 	Aqui ficam concentrados todos os conteudos das questões da atividade!
 	Veja que está separado por Parte/Questão/Item
-	
+
 	ATENÇÃO: Cada tipo possui um formato de entrada característico.
 */
 
-var Questoes = 
+var Questoes =
 [
-	{		
+	{
 		parte1_q1: //Questão 1
 		{
-			enunciadoGeral: 'Com base no valor de parcela escolhido e no montante da sua dívida inicial com a sua avó (R$ <span id="a2p1t1"></span> reais), responda:',		
-			itens: 
+			enunciadoGeral: 'Com base no valor de parcela escolhido e no montante da sua dívida inicial com a sua avó (R$ <span id="a2p1t1"></span>), responda:',		
+			itens:
 			[
 				{//A
 					enunciado: 'Se sua vó não lhe cobrasse juros, quantas parcelas você teria que pagar para zerar sua dívida para com ela?',
@@ -40,10 +40,10 @@ var Questoes =
 	{
 		parte2_q2: //Questão 2
 		{
-			itens: 
+			itens:
 			[
 				{//A
-					enunciado: 'Qual é o valor da sua dívida no final do primeiro mês?',			
+					enunciado: 'Qual é o valor da sua dívida no final do primeiro mês?',
 					tipo: 'input',
 					corrigir: corrige_q_2_a,
 					selecionada: seleciona_q_2_a,
@@ -53,17 +53,17 @@ var Questoes =
 		},
 		parte2_q3: //Questão 3
 		{
-			itens: 
+			itens:
 			[
 				{//A
-					enunciado: 'Qual é o montante da dívida antes de você pagar a segunda parcela?',			
+					enunciado: 'Qual é o montante da dívida antes de você pagar a segunda parcela?',
 					tipo: 'input',
 					corrigir: corrige_q_3_a,
 					selecionada: seleciona_q_3_a,
 					msgErro: 'Aplique o juros de 1% sobre a dívida restante, como se ela estivesse rendendo os juros da poupança.'
 				},
 				{
-					enunciado: ' E depois de pagá-la?',			
+					enunciado: ' E depois de pagá-la?',
 					tipo: 'input',
 					selecionada: seleciona_q_3_b,
 					corrigir: corrige_q_3_b,
@@ -73,17 +73,17 @@ var Questoes =
 		},
 		parte2_q4: //Questão 4
 		{
-			itens: 
+			itens:
 			[
 				{//A
-					enunciado: 'Qual é o montante da dívida antes de você pagar a terceira parcela?',			
+					enunciado: 'Qual é o montante da dívida antes de você pagar a terceira parcela?',
 					tipo: 'input',
 					corrigir: corrige_q_4_a,
 					selecionada: seleciona_q_4_a,
 					msgErro: 'Aplique mais uma vez os juros de 1% sobre a dívida restante.'
 				},
 				{//B
-					enunciado: 'E depois de pagá-la?',			
+					enunciado: 'E depois de pagá-la?',
 					tipo: 'input',
 					corrigir: corrige_q_4_b,
 					selecionada: seleciona_q_4_b,
@@ -96,16 +96,16 @@ var Questoes =
 		parte3_q5: //Questão 5
 		{
 			enunciadoGeral: 'Analisando o gráfico, responda:',
-			itens: 
+			itens:
 			[
 				{//A
-					enunciado: 'Qual é o último mês em que a dívida ainda é positiva?',			
+					enunciado: 'Qual é o último mês em que a dívida ainda é positiva?',
 					tipo: 'input',
 					corrigir: corrige_q_5_a,
 					msgErro: 'Movimente o ponto azul e verifique qual é o último mês antes da dívida ser zerada, ou seja, antes dos pontos verdes ficarem abaixo do eixo X.'
 				},
 				{//B
-					enunciado: 'Qual deve ser o valor pago na última parcela para zerar a dívida para com a sua avó?',			
+					enunciado: 'Qual deve ser o valor pago na última parcela para zerar a dívida para com a sua avó?',
 					tipo: 'input',
 					corrigir: corrige_q_5_b,
 					msgErro: 'Cheque o valor da dívida no último mês em que ela é positiva.'
@@ -114,7 +114,7 @@ var Questoes =
 		},
 		parte3_q6: //Questão 6
 		{
-			itens: 
+			itens:
 			[
 				{//A
 					enunciado: 'Qual é a diferença entre o total que você pagou e o montante emprestado?',
